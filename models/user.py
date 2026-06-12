a32a4e1306abfa74681f90a03c1ebdf2c743d673
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-12 13:53:30 +0300

    Formatted output for users_list in User.py

--- models/user.py
+++ models/user.py
@@ -1,27 +1,49 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import uuid
-from utils.helpers import ClassProperty 
+from utils.helpers import ClassProperty
 
 class User:
-    #TODO: Place dostring
-    __count = 0
+    #TODO: Place docstring
+    __users = []
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.__id = uuid.uuid4()
-        User.count += 1          
-
+        self.__id_number = uuid.uuid4()
+        User.__users.append(self)
+        
     @property
-    def id(self):
-        return self.__id
+    def id_number(self):
+        return self.__id_number
 
     @ClassProperty
     def count(cls):
-        return cls._User__count  
+        return len(cls._User__users)  
+    
+    @classmethod
+    def users_list(cls):
+        return [{user} for user in cls.__users]
+    
+    def __str__(self):
+        return f"User info:\n{self.name=},\n{self.email=}"
+    
+    def __repr__(self):
+        return f"Parent class: {self.__class__.__name__}, id_number: {self.id_number}, name: {self.name}, email: {self.email}"
 
-    @count.setter
-    def count(cls, value):       
-        cls._User__count = value
+
+
+u1 = User("Robert", "robert@email.com")
+u2 = User("John", "john@email.com")
+print(User.count)
+print(u1.id_number)
+print(u2.id_number)
+print(User.users_list())
+print(str(u1))
+
+
+
 
     
    
